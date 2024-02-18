b7dc2ef17fa6e3a812757f8c28ed82f53d6eb087
tharun1206 <tharun8011@gmail.com>
2024-02-18 13:09:16 +0000

    updated

--- user.java
+++ user.java
@@ -1,4 +1,5 @@
 i am in master branch
 hello i am in slave branch
+we are using for stash to check either its working or not
 It's contains user information which is saved in database
 and it will store cloud
