21eaf39a4c4fb4cb8f77318f6e9717ff1931d68a
tharun1206 <tharun8011@gmail.com>
2024-02-18 08:05:34 +0000

    updated both

--- access.sh
+++ access.sh
@@ -1,3 +1,4 @@
+this is for ssh
 hello ...... guys
 its all about the git access
 what are the premission need to requied
--- user.java
+++ user.java
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 i am in master branch
 hello i am in slave branch
 we are using for stash to check either its working or not
+=======
+its in master branch
+>>>>>>> 60f216d... updated in master
 It's contains user information which is saved in database
 and it will store cloud
